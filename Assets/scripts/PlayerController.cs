0d8edbd13eb53d284d76c29af3305cf85617dfce
Thomas Hickman <Thomas Hickman>
2016-08-03 11:10:04 +0100

    Initial commit

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+
+    static int speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("up"))
+        {
+            transform.Translate(Vector3.up * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("down"))
+        {
+            transform.Translate(Vector3.down * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("left"))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+        else if (Input.GetKey("right"))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+    }
+}
